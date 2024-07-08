623ef8aae142af1ad276e1871f08ca9dbec41ac1
Dostendite <dostendite@outlook.es>
2024-07-08 11:58:28 +0200

    Update Node class
    Remove redundant "set" methods and improve
    to_s method

--- lib/node.rb
+++ lib/node.rb
@@ -1,7 +1,7 @@
 class Node
   include Comparable
   attr_accessor :data, :left_child, :right_child
-  
+
   def initialize(data)
     @data = data
     @left_child = nil
@@ -9,18 +9,10 @@ class Node
   end
 
   def to_s
-    "( #{@left_child} ) <- ( #{@data} ) -> ( #{@right_child})"
+    "(#{@left_child}) <- (#{@data}) -> (#{@right_child})"
   end
 
   def <=>(other)
     @data <=> other.data
-  end
-
-  def set_left_child(data)
-    @left_child = data
-  end
-
-  def set_right_child(data)
-    @right_child = data
   end
 end
