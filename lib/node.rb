47106aa6316376de4a95028caead6d4dc4cd6677
Dostendite <dostendite@outlook.es>
2024-07-11 02:32:37 +0200

    Simplify node child names
    left_child -> left
    right_child -> right

--- lib/node.rb
+++ lib/node.rb
@@ -1,17 +1,18 @@
 class Node
   include Comparable
-  attr_accessor :data, :left_child, :right_child
+  attr_accessor :data, :left, :right
 
   def initialize(data)
     @data = data
-    @left_child = nil
-    @right_child = nil
+    @left = nil
+    @right = nil
   end
 
   def to_s
-    "(#{@left_child}) <- (#{@data}) -> (#{@right_child})"
+    "(#{@left}) <- (#{@data}) -> (#{@right})"
   end
 
+  # this might be a bad practice, do some research
   def <=>(other)
     @data <=> other.data
   end
